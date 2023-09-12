03d6cc7165ab3bad33e1f0669024efc0fab8b5d6
Roman Gutierrez <124226629+Romangutierrez@users.noreply.github.com>
2023-09-12 20:05:19 +0800

    stud-<RomanP.Gutierrez.js

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,11 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
 }
